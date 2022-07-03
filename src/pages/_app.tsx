ac1581ede7347c85e6df0092eb2fcb587545e1c3
Corey DeLoach <cdeloach16@gmail.com>
2022-07-03 15:53:12 -0500

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,66 @@
+// src/pages/_app.tsx
+import { withTRPC } from "@trpc/next";
+import type { AppRouter } from "@/server/router";
+import type { AppType } from "next/dist/shared/lib/utils";
+import superjson from "superjson";
+import { SessionProvider } from "next-auth/react";
+import { loggerLink } from "@trpc/client/links/loggerLink";
+import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
+import "@/styles/globals.css";
+
+const MyApp: AppType = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") {
+    return "";
+  }
+  if (process.browser) return ""; // Browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
+export default withTRPC<AppRouter>({
+  config({ ctx }) {
+
+    const url = `${getBaseUrl()}/api/trpc`;
+
+    const links = [
+      loggerLink(),
+      httpBatchLink({
+        maxBatchSize: 10,
+        url,
+      }),
+    ];
+    return {
+      queryClientConfig: {
+        defaultOptions: {
+          queries: {
+            staleTime: 60,
+          },
+        },
+      },
+      headers() {
+        if (ctx?.req) {
+          return {
+            ...ctx.req.headers,
+            "x-ssr": "1",
+          };
+        }
+        return {};
+      },
+      links,
+      transformer: superjson,
+    };
+  },
+  ssr: false,
+})(MyApp);
